ca5ea9f3dc6c101f7977e942ed88a55dc251c00f
Jason S. Jones <jsjones96@gmail.com>
2019-10-17 17:42:18 -0700

    feat: add  production scripts and config decisions

--- src/utils/createDbConnection.ts
+++ src/utils/createDbConnection.ts
@@ -1,6 +1,13 @@
 import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 
 export const createDbConnection = async (): Promise<Connection> => {
-    const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-    return createConnection({ ...connectionOptions, name: 'default' });
+    const env = process.env.NODE_ENV;
+    if (env !== 'production') {
+        const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+        return createConnection({ ...connectionOptions, name: 'default' });
+    }
+    return createConnection({
+        type: 'postgres',
+        url: process.env.DATABASE_URL
+    });
 };
