64321f0159e1b0114498a429ab457f5ce36be468
Jason S. Jones <jsjones96@gmail.com>
2019-10-17 19:01:09 -0700

    fix: conditionally assign port number

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 import { createDbConnection } from './utils/createDbConnection';
 import app from './app';
 
+const PORT = process.env.PORT || 3000;
+
 createDbConnection().then(() => {
-    app.listen(3000, () => console.log('express server running at http://localhost:3000'));
+    app.listen(PORT, () => console.log(`express server running at http://localhost:${PORT}`));
 });
